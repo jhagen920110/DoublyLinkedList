d0c882400fc8fc1b34c991d4f2628364476f060b
JONATHAN HAGEN <jhagen920110@gmail.com>
2017-01-29 03:41:53 -0800

    Update CircularHeadList.cpp

--- CircularHeadList.cpp
+++ CircularHeadList.cpp
@@ -6,55 +6,60 @@ using namespace std;
 
 CHList::CHList()
 { 
-Node * dnode = new Node('?');
-dummy_node=dnode;
-listSize=0;
-first=dummy_node;
-first->next=first;
+	Node * dnode = new Node('?');
+	dummy_node=dnode;
+	listSize=0;
+	first=dummy_node;
+	first->next=first;
 }
 
 bool CHList::empty()
-{if (first->next==first)
-	return 1;
-else
-	return 0;
+{
+	if (first->next==first)
+		return 1;
+	else
+		return 0;
 }
 
 void CHList::insert(Node * newnode, Node * pre)
 { 
 	Node * temp;
 	if ((empty())&& (pre!=dummy_node))
-	{cout<<"*&$MU##\n";
+	{
+		cout<<"*&$MU##\n";
 	}
 	else
 	{
-	temp=pre->next;
-	pre->next=newnode;
-	newnode->next=temp;
-	listSize++;
+		temp=pre->next;
+		pre->next=newnode;
+		newnode->next=temp;
+		listSize++;
 	}
 }
  
 
 void CHList::erase(Node * prenode)
-{Node *to_be_delete;
- Node *suc;
- if (!empty())
- {
- to_be_delete=prenode->next;
- suc=to_be_delete->next;
- prenode->next=suc;
- to_be_delete->next='\0';
- listSize--;
- }
+{
+	Node *to_be_delete;
+	 Node *suc;
+	 if (!empty())
+ 	{
+	 to_be_delete=prenode->next;
+	 suc=to_be_delete->next;
+	 prenode->next=suc;
+	 to_be_delete->next='\0';
+	 listSize--;
+ 	}
 }
 
 void CHList::display()
-{Node * current_node;
- current_node=first;
- do
- {cout<<current_node->data<<" ";
- current_node=current_node->next;
- }while (current_node!=first);
-cout<<'\n';
+{
+	Node * current_node;
+	 current_node=first;
+	 do
+ 	{
+		 cout<<current_node->data<<" ";
+ 		current_node=current_node->next;
+ 	}while (current_node!=first);
+	cout<<'\n';
 }
